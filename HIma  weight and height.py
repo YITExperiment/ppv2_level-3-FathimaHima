14f1fb2ed96a49f42d1d407d12aeb36a4f2c9437
FathimaHima <134586414+FathimaHima@users.noreply.github.com>
2023-05-25 12:49:21 +0530

    Add files via upload

---
+++ HIma  weight and height.py
@@ -0,0 +1,22 @@
+Python 3.11.3 (tags/v3.11.3:f3909b8, Apr  4 2023, 23:49:59) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+==================================== RESTART: C:/Users/AL AZHAR/AppData/Local/Programs/Python/Python311/hima name.py ===================================
+Hello,World!
+What is your name?Hima
+Hello Hima
+>>> age=15
+>>> priny(age)
+Traceback (most recent call last):
+  File "<pyshell#1>", line 1, in <module>
+    priny(age)
+NameError: name 'priny' is not defined. Did you mean: 'print'?
+>>> age=15
+>>> print(age)
+15
+>>> weight=61
+>>> print(weight)
+61
+>>> height=159
+>>> print(height)
+159
